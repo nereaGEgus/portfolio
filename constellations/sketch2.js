c6e6db11e02d067d13002145ebc372ebcef23f86
nereaGEgus <144602634+nereaGEgus@users.noreply.github.com>
2023-09-15 17:36:49 +0200

    particles2

--- constellations/sketch2.js
+++ constellations/sketch2.js
@@ -51,22 +51,22 @@ class Particle {
 }
 
 // an array to add multiple particles
-let particles = [];
+let prtls = [];
 
 function setup() {
   const canvas = createCanvas(windowWidth, windowHeight);
 	canvas.parent("home-back");
   for(let i = 0;i<width/4;i++){
-    particles.push(new Particle());
+    prtls.push(new Particle());
   }
 }
 
 function draw() {
   background('#0f0f0f');
-  for(let i = 0;i<particles.length;i++) {
-    particles[i].createParticle();
-    particles[i].moveParticle();
-		particles[i].attract(mouseX,mouseY);
-    particles[i].joinParticles(particles.slice(i));
+  for(let i = 0;i<prtls.length;i++) {
+    prtls[i].createParticle();
+    prtls[i].moveParticle();
+		prtls[i].attract(mouseX,mouseY);
+    prtls[i].joinParticles(prtls.slice(i));
   }
 }
